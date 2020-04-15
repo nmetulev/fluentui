c4777911e096f92eac90daec378320ae83f59a7d
Nikola Metulev <nikolame@microsoft.com>
2020-04-14 21:06:03 -0700

    starting to add initial web components

---
+++ packages/fluent-web-components/button/README.md
@@ -0,0 +1,11 @@
+# @fluentui/fluent-button
+
+**FluentButton components for [Fluent UI React](https://developer.microsoft.com/en-us/fluentui)**
+
+These are not production-ready components and **should never be used in product**. This space is useful for testing new components whose APIs might change before final release.
+
+To import FluentButton components:
+
+```js
+import { ComponentName } from '@fluentui/fluent-button';
+```
---
+++ packages/fluent-web-components/icon/src/fluent-icon.ts
@@ -0,0 +1,39 @@
+import { customElement, html, LitElement, property } from 'lit-element';
+import { styles } from './fluent-icon-css';
+
+/**
+ * Represents an Icon
+ *
+ * @export
+ * @class fluent-icon
+ */
+@customElement('fluent-icon')
+export class FluentIcon extends LitElement {
+  /**
+   * Array of styles to apply to the element. The styles should be defined
+   * using the `css` tag function.
+   */
+  static get styles() {
+    return styles;
+  }
+
+  /**
+   * Fabric Core Icon Name
+   * https://developer.microsoft.com/en-us/fabric#/styles/web/icons
+   *
+   * @type {string}
+   * @memberof FluentIcon
+   */
+  @property({ attribute: 'name' }) public name: string;
+
+  /**
+   * Invoked on each update to perform rendering tasks. This method must return
+   * a lit-html TemplateResult. Setting properties inside this method will *not*
+   * trigger the element to update.
+   */
+  protected render() {
+    return html`
+      <i class="ms-Icon ms-Icon--${this.name}" aria-hidden="true"></i>
+    `;
+  }
+}
